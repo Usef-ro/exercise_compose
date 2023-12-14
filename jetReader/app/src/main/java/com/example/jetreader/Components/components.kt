64061f8c6c1b45900b87990a7848da0789f48fc8
usef <jo.joy31@yahoo.com>
2023-12-14 01:20:58 -0800

    fix Login and Sign Up Form

--- jetReader/app/src/main/java/com/example/jetreader/Components/components.kt
+++ jetReader/app/src/main/java/com/example/jetreader/Components/components.kt
@@ -1,7 +1,16 @@
 package com.example.jetreader.Components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -9,6 +18,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -16,26 +26,34 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetreader.R
 
+
+/*
+Logo
+ */
 @Composable
 fun readerLogo(modifier: Modifier = Modifier) {
     Text(
         modifier = modifier.padding(16.dp),
         text = " A . Reader",
-        style = MaterialTheme.typography.bodyMedium,
+        style = MaterialTheme.typography.bodyLarge,
         color = Color.Red.copy(alpha = 0.5f)
     )
 
 }
 
-
+/*
+Input Field
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InputField(
@@ -71,7 +89,9 @@ fun InputField(
 
 }
 
-
+/*
+Password Input
+ */
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun passwordInput(
@@ -118,8 +138,71 @@ fun PasswordVisibility(passwordVisibility: MutableState<Boolean>) {
     val visible = passwordVisibility.value
     IconButton(onClick = { passwordVisibility.value = !visible }) {
         Icon(
-            painter = painterResource(id = R.drawable.baseline_remove_red_eye_24),
+            painter = if (passwordVisibility.value)
+                painterResource(id = R.drawable.visible)
+            else painterResource(id = R.drawable.invisible),
             contentDescription = "show pwd"
         )
+    }
+}
+
+
+/*
+ItemCard
+ */
+
+@Preview(showBackground = true)
+@Composable
+fun ItemCard() {
+
+    Surface(
+        shadowElevation = 15.dp,
+        modifier = Modifier
+            .padding(10.dp)
+            .height(90.dp)
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.onPrimary),
+        border = BorderStroke(1.dp, color = Color.Gray.copy(1f)),
+        shape = RoundedCornerShape(10.dp, 10.dp, 10.dp, 10.dp)
+    ) {
+
+
+        Row(
+            modifier = Modifier.padding(5.dp)
+        ) {
+
+
+            Surface(
+                modifier = Modifier.size(80.dp),
+                shape = RoundedCornerShape(10.dp, 10.dp, 10.dp, 10.dp)
+            ) {
+                Image(
+                    modifier = Modifier.fillMaxSize(),
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = ""
+                )
+            }
+
+
+            Column(
+                modifier = Modifier
+                    .weight(0.3f)
+                    .padding(start = 15.dp)
+            ) {
+
+                Text(
+                    text = "Title",
+                    style = TextStyle(fontWeight = FontWeight.Bold, color = Color.Black)
+                )
+
+                Text(
+                    text = "Description",
+                    style = TextStyle(fontWeight = FontWeight.ExtraLight, color = Color.Black)
+                )
+            }
+
+        }
+
+
     }
 }
--- jetReader/app/src/main/java/com/example/jetreader/Screens/login/readerBookLogin.kt
+++ jetReader/app/src/main/java/com/example/jetreader/Screens/login/readerBookLogin.kt
@@ -2,15 +2,26 @@ package com.example.jetreader.Screens.login
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -22,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -37,6 +49,10 @@ Reader Login
 @Composable
 fun readerBookLogin(navController: NavController) {
 
+    val showLogin = rememberSaveable {
+        mutableStateOf(true)
+    }
+
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -45,9 +61,50 @@ fun readerBookLogin(navController: NavController) {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
+            /*
+            Logo
+             */
+
             readerLogo()
-            userForm(isCreateAccount = false) { email, pwd ->
-                Log.e("Inputss", "readerBookLogin: $email ,, $pwd")
+
+
+            /*
+            Form
+             */
+            if (showLogin.value)
+                userForm(loading = false, isCreateAccount = showLogin.value) { email, pwd ->
+                    Log.e("Inputss", "readerBookLogin: $email ,, $pwd")
+                }
+            else {
+                userForm(loading = false, isCreateAccount = showLogin.value) { email, pwd ->
+
+                }
+
+            }
+
+            Spacer(
+                modifier = Modifier.height(15.dp),
+            )
+
+            /*
+            Login or create
+             */
+            Row(
+                modifier = Modifier.padding(15.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                val text = if (showLogin.value) "Sign Up" else "Login"
+                Text(text = "New User?")
+                Text(
+                    text = text, modifier = Modifier
+                        .padding(10.dp)
+                        .clickable {
+                            showLogin.value = !showLogin.value
+                        }, fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.secondary
+                )
             }
         }
     }
@@ -95,6 +152,15 @@ fun userForm(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+        if (isCreateAccount) {
+            Text(
+                text = "Please enter a valid email and password",
+                modifier = Modifier.padding(4.dp)
+            )
+        } else {
+            Text(text = "")
+        }
+
         /*
         Email
          */
@@ -119,6 +185,50 @@ fun userForm(
             }
         )
 
+
+        /*
+        Button Submit
+         */
+        submitButton(
+            textId = if (isCreateAccount) {
+                "Create Account"
+
+            } else {
+                "Login"
+            },
+            loading = loading, validInput = valid
+        ) {
+
+            onDone(email.value.trim(), password.value.trim())
+            keyboardController?.hide()
+
+        }
+    }
+}
+
+
+@Composable
+fun submitButton(textId: String, loading: Boolean, validInput: Boolean, onClick: () -> Unit) {
+
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .padding(3.dp)
+
+            .fillMaxWidth(),
+        enabled = !loading && validInput,
+        shape = RoundedCornerShape(10.dp,10.dp,10.dp,10.dp),
+
+    ) {
+        if (loading) CircularProgressIndicator(
+            modifier = Modifier
+                .padding(10.dp)
+                .size(25.dp)
+        ) else Text(text = textId, modifier = Modifier
+            .padding(2.dp)
+            .clickable {
+
+            })
     }
 }
 
